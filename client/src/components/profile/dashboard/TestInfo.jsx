e968b7f7fa84133e585d7194e04ba97210132479
Dnyaneshwar <dnyaneshwarpk20@gmail.com>
2024-04-11 23:49:33 +0530

    feat: result pdf downloading done

--- client/src/components/profile/dashboard/TestInfo.jsx
+++ client/src/components/profile/dashboard/TestInfo.jsx
@@ -1,7 +1,5 @@
 export default function TestInfo({ test, index }) {
-  return <div className="my-4">
-    <p className="leading-3 font-semibold">{index + 1}&#41; {test.title} - {test.availableAt.substring(0, 10).split("-").join(" / ")}</p>
-    <span className="text-slate-400 text-[12px]">LSAT Study / {test.questions.length} problems</span>
-    {/* </div> */}
+  return <div className="my-6">
+    <p className="leading-3 font-[400]">{index + 1}&#41; {test.title} - {test.availableAt.substring(0, 10).split("-").join(" / ")}</p>
   </div>
 }
--- client/src/components/profile/results/PreviousResults.jsx
+++ client/src/components/profile/results/PreviousResults.jsx
@@ -4,15 +4,30 @@ import useGetTest from "../../../hooks/useGetTest";
 import Error from "../../Error";
 import Loader from "../../Loader";
 import { useSelector } from "react-redux";
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 export default function PreviousResults() {
   const [test, setTest] = useState({});
   const [error, setError] = useState();
   const [result, setResult] = useState({});
   const { testId } = useParams();
-  const { _id } = useSelector(store => store.user)
+  const { user } = useSelector(store => store)
 
-  const { loading, getTest } = useGetTest()
+  const { loading, getTest } = useGetTest();
+
+  function downloadPDF() {
+    const capture = document.querySelector('.result-container');
+    html2canvas(capture).then((canvas) => {
+      const imgData = canvas.toDataURL('img/png');
+      const doc = new jsPDF('p', 'mm', 'a4');
+      const componentWidth = 200;
+      const componentHeight = 100;
+      doc.addImage(imgData, 'PNG', 0, 0, componentWidth, componentHeight);
+      doc.save('receipt.pdf');
+    });
+  }
+
   useEffect(function () {
     async function retireve() {
       try {
@@ -26,12 +41,12 @@ export default function PreviousResults() {
     }
 
     retireve();
-  }, [])
+  }, []);
 
   useEffect(function () {
     async function retrieve() {
       try {
-        const response = await fetch(`http://localhost:3000/test/completedTestsByuser/${_id}`);
+        const response = await fetch(`http://localhost:3000/test/completedTestsByuser/${user._id}`);
         const data = await response.json();
         if (response.status) {
           const requiredData = data.find(res => res.testId = testId)
@@ -47,33 +62,39 @@ export default function PreviousResults() {
   }, [])
 
   return <div>
-    <h2>{test?.title}</h2>
     {error && <Error message={error} setter={setError} />}
-
     <p className="max-w-[70ch]">You can upload a key to use for grading. You can upload a .csv file with student responses and scores, or a .txt file with student responses only.</p>
-    {test._id && <table className="mt-10">
-      <tbody>
-        <tr>
-          <td className="pt-4 pr-4"><strong>Duration:</strong>  {test?.testDuration} mins</td>
-          <td><strong>Total Marks:</strong> {test.highestMarks}</td>
-        </tr>
-        <tr>
-          <td className="pr-4"><strong>Test Status:</strong> {test?.isReleased ? test.passingMarks <= result.score ? "Passed" : "Failed" : "Test answers not released yet"}</td>
-          {test.isReleased && <td><strong>Marks Earned:</strong> {result.score}</td>}
-        </tr>
-        {test.isReleased && <tr>
-          <td className="pr-4"><strong>Correct Answer:</strong> {result.score}/{test?.questions?.length}</td>
-          <td><strong>Wrong Answer:</strong> {test?.questions?.length - result.score}/{test?.questions?.length}</td>
-        </tr>}
-        <tr>
-          <td className="pr-4"><strong>Attempt:</strong> {test?.questions?.length}/{test?.questions?.length}</td>
-          <td><strong>Skipped:</strong> 0/{test?.questions?.length}</td>
-        </tr>
-      </tbody>
-    </table>}
+    <div className="result-container w-full h-auto flex items-center justify-center">
+      <div>
+        <h2 className="mt-10">{test?.title}</h2>
+        <h3 className="my-4">Candidate Information</h3>
+        <p><strong>Name : </strong>{user.name}</p>
+        <p><strong>Division : </strong>{user.division}</p>
+        {test._id && <table className="mt-10">
+          <tbody>
+            <tr>
+              <td className="pt-4 pr-4"><strong>Duration:</strong>  {test?.testDuration} mins</td>
+              <td><strong>Total Marks:</strong> {test.highestMarks}</td>
+            </tr>
+            <tr>
+              <td className="pr-4"><strong>Test Status:</strong> {test?.isReleased ? test.passingMarks <= result.score ? "Passed" : "Failed" : "Test answers not released yet"}</td>
+              {test.isReleased && <td><strong>Marks Earned:</strong> {result.score}</td>}
+            </tr>
+            {test.isReleased && <tr>
+              <td className="pr-4"><strong>Correct Answer:</strong> {result.score}/{test?.questions?.length}</td>
+              <td><strong>Wrong Answer:</strong> {test?.questions?.length - result.score}/{test?.questions?.length}</td>
+            </tr>}
+            <tr>
+              <td className="pr-4"><strong>Attempt:</strong> {test?.questions?.length}/{test?.questions?.length}</td>
+              <td><strong>Skipped:</strong> 0/{test?.questions?.length}</td>
+            </tr>
+          </tbody>
+        </table>}
+      </div>
+    </div>
 
     {loading && <Loader />}
 
-    <button className="btn-tertiary mt-8">Download Report</button>
+    <button className="btn-tertiary mt-8" onClick={downloadPDF}>Download Report</button>
   </div >
 }
